b9ef5990129e47b095b8886b30e0922b4bbb8e22
Moises <moisesfreitas1@outlook.com>
2017-04-08 22:56:26 -0300

    q3

---
+++ Q3.py
@@ -0,0 +1,21 @@
+dono1 = 'Artur'
+dono2 = 'Bernardo'
+dono3 = 'Cesar'
+
+carro1 = 'Palio'
+carro2 = 'Gol'
+carro3 = 'Ford Fusion'
+
+cor1 = 'cinza'
+cor2 = 'verde'
+cor3 = 'azul'
+
+combinacao1 = [dono1,carro1, cor1]
+combinacao2 = [dono3,carro3,cor2]
+combinacao3 = [dono2,carro2,cor3]
+
+
+print("Carro: ",combinacao1[1], " Cor: ", combinacao1[2])
+print("Carro: ",combinacao2[1], " Cor: ", combinacao2[2])
+print("Carro: ",combinacao3[1], " Cor: ", combinacao3[2])
+
