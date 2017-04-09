c05ea6138ebc0922958459e982dcff154a9b5451
Moises <moisesfreitas1@outlook.com>
2017-04-09 08:52:52 -0300

    q3

--- Q2.py
+++ Q2.py
@@ -5,8 +5,10 @@ pesoTotal = 245
 
 margem1 = [50, 75, 120, 0]
 margem2 = [0, 0, 0, 0]
+PesoMagem2 = margem2[0]+margem2[1]+margem2[2]+margem2[3]
+tentativa = 0
 
-while (margem2[0]+margem2[1]+margem2[2]+margem2[3]) < pesoTotal:
+while PesoMagem2 < pesoTotal:
     rand1 = random.randint(0, 3)
     rand2 = random.randint(0, 3)
     while rand1 == rand2:
@@ -21,33 +23,26 @@ while (margem2[0]+margem2[1]+margem2[2]+margem2[3]) < pesoTotal:
     margem2[rand2] = margem1[rand2]
     margem1[rand2] = 0
 
-    rand1 = random.randint(0, 3)
-    rand2 = random.randint(0, 3)
-    while rand1 == rand2:
-        rand2 = random.randint(0, 3)
-    while (margem2[rand1] + margem2[rand2]) > capacidadeMax:
-        rand1 = random.randint(0, 3)
-        rand2 = random.randint(0, 3)
-        while rand1 == rand2:
-            rand2 = random.randint(0, 3)
-    margem1[rand1] = margem2[rand1]
-    margem2[rand1] = 0
-    margem1[rand2] = margem2[rand2]
-    margem2[rand2] = 0
+    PesoMagem2 = margem2[0] + margem2[1] + margem2[2] + margem2[3]
 
-    rand1 = random.randint(0, 3)
-    rand2 = random.randint(0, 3)
-    while rand1 == rand2:
-        rand2 = random.randint(0, 3)
-    while (margem1[rand1] + margem1[rand2]) > capacidadeMax:
+    if PesoMagem2 < pesoTotal:
         rand1 = random.randint(0, 3)
         rand2 = random.randint(0, 3)
         while rand1 == rand2:
             rand2 = random.randint(0, 3)
-    margem2[rand1] = margem1[rand1]
-    margem1[rand1] = 0
-    margem2[rand2] = margem1[rand2]
-    margem1[rand2] = 0
+        while (margem2[rand1] + margem2[rand2]) > capacidadeMax:
+            rand1 = random.randint(0, 3)
+            rand2 = random.randint(0, 3)
+            while rand1 == rand2:
+                rand2 = random.randint(0, 3)
+        margem1[rand1] = margem2[rand1]
+        margem2[rand1] = 0
+        margem1[rand2] = margem2[rand2]
+        margem2[rand2] = 0
+
+    PesoMagem2 = margem2[0] + margem2[1] + margem2[2] + margem2[3]
+    tentativa = tentativa + 1
+    print(tentativa,"a tentativa")
 
 PesoMagem2 = margem2[0]+margem2[1]+margem2[2]+margem2[3]
 PesoMagem1 = margem1[0]+margem1[1]+margem1[2]+margem1[3]
